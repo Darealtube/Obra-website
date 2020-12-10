9f94d2e332d8fb7ba5dceac0578ee35e610938a9
Darealtube <evajavier143@gmail.com>
2020-12-10 15:32:00 +0800

    Material UI patch v1.0.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,131 @@
-import Link from 'next/link'
-import {Nav, Form, Button, Alert, Image} from 'react-bootstrap'
+import Link from "next/link";
+import React from "react";
+import {
+  Toolbar,
+  CssBaseline,
+  Paper,
+  Grid,
+  Typography,
+  Button,
+  TextField,
+  makeStyles,
+  Box,
+} from "@material-ui/core";
+import PaletteIcon from "@material-ui/icons/Palette";
 
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(https://picsum.photos/500)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Login = () => {
+  const classes = useStyles();
+
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © "}
+        <Link href="#">Canvas</Link> {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
+
   return (
-    <div style={{backgroundImage: "url('https://i.ytimg.com/vi/9LqaZ6C2bvI/maxresdefault.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
-<div className="login-form">
-<Nav className="flex-column">
-    <Form style={{"margin": "1em"}}>
-        <Form.Group controlId="formEmail">
-            <Form.Label><h5>Email</h5></Form.Label>
-            <Form.Control type="email" placeholder="Enter email" ></Form.Control>
-        </Form.Group>
+    <div>
+      <Grid container className={classes.root}>
+        <CssBaseline />
 
-        <Form.Group controlId="formPassword">
-            <Form.Label><h5>Password</h5></Form.Label>
-            <Form.Control type="password" placeholder="Enter password" ></Form.Control>
-        </Form.Group>
-        <br/>
-        <Button type="submit" variant="dark" size="lg"> <Link href="/home">Log In</Link></Button>
-    </Form>
-    <br/>
-    <Alert variant="info">Don't have an account? <Alert.Link variant="dark"><Link href="/register">Register now!</Link></Alert.Link></Alert>
-</Nav>
-</div>
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
 
-<style jsx>{`
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Toolbar>
+              <PaletteIcon fontSize="large" />
+              <Typography variant="h4" color="inherit" noWrap>
+                Canvas
+              </Typography>
+            </Toolbar>
+            <br />
+            <Typography variant="h5">Log In</Typography>
 
- .login-form{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: linear-gradient(65deg, #5e60ce, #5390d9);
-    width: 30vw;
-    height: 100vh;
-    text-align: center;
-    outline:none;
- }
+            <form className={classes.form}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                color="primary"
+              />
 
- *{
-     display:flex;
-     flex-direction: row;
- }
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                name="password"
+                autoComplete="password"
+                autoFocus
+                color="primary"
+                helperText="Don't share your password to anyone."
+              />
 
-`}
-</style>
-</div>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                <Link href="/home">
+                  <span style={{ color: "white" }}>Log In</span>
+                </Link>
+              </Button>
+            </form>
+            <Grid container>
+              <Grid item xs={6}>
+                <Typography color="secondary">
+                  <Link href="#">Forgot Password?</Link>
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography color="primary">
+                  <Link href="/register">Don't have an account? Sign up!</Link>
+                </Typography>
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
   );
-}
+};
 
 export default Login;
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,202 +1,201 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import { Form, Container, Row, Col, Button} from 'react-bootstrap'
+import Link from "next/link";
+import React from "react";
+import {
+  Toolbar,
+  CssBaseline,
+  Paper,
+  Grid,
+  Typography,
+  Button,
+  TextField,
+  makeStyles,
+  Box,
+} from "@material-ui/core";
+import PaletteIcon from "@material-ui/icons/Palette";
 
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(https://picsum.photos/500)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(2, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Register = () => {
+  const classes = useStyles();
 
-return(
-<div className="register-div">
- <Head>
- <meta name="viewport" content="width=device-width, initial-scale=1" />
- </Head>
- <div className="register-form">
-    <Container fluid className="form">
-        <div className="title">
-        Canvas
-        </div>
-        <br/>
-        <Form>
-            <Form.Group controlId="register1">
-            <Form.Row>
-            <Col xs={6}>
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="Enter Username"></Form.Control>
-            </Col>
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © "}
+        <Link href="#">Canvas</Link> {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
 
-            <Col xs={6}>
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter Email"></Form.Control>
-            </Col>
-            </Form.Row>
-            </Form.Group>
+  return (
+    <div>
+      <Grid container className={classes.root}>
+        <CssBaseline />
 
-            <Form.Group controlId="register2">
-            <Form.Row>
-                <Col xs={4}>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="text"></Form.Control>
-                </Col>
-                <Col xs={2}>
-                    <Form.Label>Age</Form.Label>
-                    <Form.Control type="number" min="13" max="100"></Form.Control>
-                </Col>
-                <Col xs={6}>
-                    <Form.Label>Phone Number</Form.Label>
-                    <Form.Control placeholder="Optional" />
-                </Col>
-            </Form.Row>
-            </Form.Group>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Toolbar>
+              <PaletteIcon fontSize="large" />
+              <Typography variant="h4" color="inherit" noWrap>
+                Canvas
+              </Typography>
+            </Toolbar>
+            <br />
+            <Typography variant="h5">Log In</Typography>
 
-            <Form.Group controlId="register3">
-            <Form.Row>
-                <Col xs={6}>
-                    <Form.Label>Country</Form.Label>
-                    <Form.Control type="text"></Form.Control>
-                </Col>
-                <Col xs={6}>
-                    <Form.Label>City/State</Form.Label>
-                    <Form.Control type="text"></Form.Control>
-                </Col>
-            </Form.Row>
-            </Form.Group>
-              <br/>
-            <Form.Group controlId="register4">
-            <Form.Row>
-                <Col xs={6}>
-                <Form.Control as="select">
-                    <option>What kind of art peaks your interest?</option>
-                </Form.Control>
-                </Col>
-                <Col xs={6}>
-                <Form.Control as="select">
-                    <option>What kind of art do you do?</option>
-                    <option>I do not make art.</option>
-                </Form.Control>
-                </Col>
-            </Form.Row>
-            </Form.Group>
-                <br/>
-            <Form.Group controlId="register5">
-            <Form.Row>
-                <Col xs={6}>
-                <Form.File id="exampleFormControlFile1" label="Choose your Profile Picture" />
-                </Col>
-                <Col xs={6}>
-                    <div id="image-display"></div>
-                </Col>
-            </Form.Row>
-            </Form.Group>
-                <br/>
-            <Form.Group className="special" controlId="register6">
-            <Form.Row>
-                <Col>
-                <Form.Label>Set your Profile Description</Form.Label>
-                <Form.Control as="textarea" rows={3} style={{resize: "none"}}></Form.Control>
-                </Col>
-            </Form.Row>
-            </Form.Group>
-                <br/>
-            <Form.Row style={{textAlign: "center"}}>
-                <Col xs={6}>
-                <Button  variant="dark" className="submit"><Link href="/home">Register</Link></Button>
-                </Col>
-                <Col xs={6}>
-                <Button  variant="dark" className="submit"><Link href="/">Login</Link></Button>
-                </Col>
-            </Form.Row>
-        </Form>
-    </Container>
- </div>
-<style tsx>{`
-    .register-form{
-        background: white;
-        width: 40vw;
-        height: 100vh;
-        font-size: 1rem;
-        overflow: auto;
-    }
+            <form className={classes.form}></form>
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoFocus
+                  color="primary"
+                />
+              </Grid>
 
-    .form{
-        margin: 1em 0 2em;
-    }
+              <Grid item xs={6}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  name="password"
+                  autoFocus
+                  color="primary"
+                />
+              </Grid>
 
-    .register-div{
-        background-image: url('https://i.pinimg.com/originals/2d/80/eb/2d80eba2c91f86f6ca21834684cc2d25.jpg');
-        background-size: cover;
-        background-repeat: no-repeat;
-        width: 100vw;
-        height: 100vh;
-        position: fixed;
-    }
+              <Grid item xs={4}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  autoFocus
+                  color="primary"
+                />
+              </Grid>
 
-    .submit{
-        width: 40vw;
-    }
+              <Grid item xs={4}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="age"
+                  label="Age"
+                  name="age"
+                  autoFocus
+                  color="primary"
+                />
+              </Grid>
 
-    @media all and (min-width: 17.5em){
-        .register-div{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        }
+              <Grid item xs={4}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  id="phone"
+                  label="Phone Number"
+                  name="phone"
+                  color="primary"
+                  helperText="Optional."
+                />
+              </Grid>
 
-        .register-form{
-            width: 95vw;
-        }
+              <Grid item xs={6}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="country"
+                  label="Country"
+                  name="country"
+                  autoFocus
+                  color="primary"
+                />
+              </Grid>
 
-        #register2.form-control{
-            padding: 3px 6px;
-        }
-    }
+              <Grid item xs={6}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="city"
+                  label="City/State"
+                  name="city"
+                  autoFocus
+                  color="primary"
+                />
+              </Grid>
 
-    @media all and (min-width: 48em){
-        .register-div{
-            display: block;
-        }
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  <Link href="/home">
+                    <span style={{ color: "white" }}>Register</span>
+                  </Link>
+                </Button>
+              </Grid>
 
-        .register-form{
-            width: 60vw;
-            font-size: 1.4em;
-        }
+              <Grid item xs={12}>
+                <Typography>
+                  <Link href="/">Already have an account? Log in!</Link>
+                </Typography>
+              </Grid>
+            </Grid>
+          </div>
+          <Box mt={5}>
+            <Copyright />
+          </Box>
+        </Grid>
 
-        .submit{
-            width: 20vw;
-            height: 15vh;
-            font-size: 1.2em;
-        }
-    }
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      </Grid>
+    </div>
+  );
+};
 
-    @media all and (min-width: 64em){
-        .register-form{
-            font-size: 1.5em;
-            line-height: 2em;
-        }
-        #register6.form-control{
-            height: 15vh;
-            font-size: 1.5em;
-        }
-        .title{
-            font-size: 2em;
-            line-height: 2em;
-        }
-
-        .submit{
-            height: 8vh;
-        }
-    }
-
-    @media all and (min-width: 85.375em){
-        .register-form{
-            font-size: 1em;
-            width: 50vw;
-        }
-    }
-`}
-</style>
-</div>
-)
-}
-
-export default Register
+export default Register;

7be656df7b7777209cb73d7c476f52c0e42c661e
Darealtube <evajavier143@gmail.com>
2020-12-21 15:27:41 +0800

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   env: {
-    MONGODB_URI:
-      "mongodb+srv://New_John_Doe:tk9YLFaCL1PHxYRu@cluster0.kkxjp.mongodb.net/post?retryWrites=true&w=majority",
+    MONGODB_URI: "",
   },
 };

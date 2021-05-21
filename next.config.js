86e52cf23aa33c8ce5b85389374ce6ac3948fdcc
Darealtube <evajavier143@gmail.com>
2021-05-21 15:50:01 +0800

    Test Update

--- next.config.js
+++ next.config.js
@@ -12,7 +12,6 @@ module.exports = {
     {
       src: "/.*",
       headers: {
-        "Access-Control-Allow-Origin": "*",
         "X-Content-Type-Options": "nosniff",
         "X-XSS-Protection": "1",
       },
